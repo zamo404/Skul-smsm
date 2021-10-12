86a9554835df08b60963f4a42c2c7822501eb8d7
Glitch (bejewled-halved-stock) <none>
2021-10-12 11:05:32 +0000

    🙅🖇 Checkpoint ./Ekonomi/cash.js:33112061/138 ./ayarlar.json:33112061/2

--- Ekonomi/cash.js
+++ Ekonomi/cash.js
@@ -6,21 +6,21 @@ exports.run = async (client, message, args) => {
           
   var cüzdan = db.fetch(`para_${user.id}`)
   var banka = db.fetch(`bankapara_${user.id}`)   
-//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
   var toplam= cüzdan+banka
 message.channel.send(new Discord.MessageEmbed()
-                  .setColor("YELLOW")
-                  .setFooter('Kraken Balance Info!')
+                  .setColor("GREEN")
+                  .setFooter('Jungle Balance Info!')
                   .setThumbnail('https://media.discordapp.net/attachments/852987040659931248/871313810441510922/image0.gif')
                   .setAuthor(user.tag, user.avatarURL({dynamic: true}))     
-                  .addField(`Kraken Cash <a:emoji_56:874712610396844135> `,`__${toplam ? toplam +'':`0`}__`,true))
+                  .addField(`Jungle Cash <a:emoji_56:874712610396844135> `,`__${toplam ? toplam +'':`0`}__`,true))
                     
   }
 
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
 exports.conf = {
   enabled: true,
-    aliases: ["cash","money",'credit','c'],
+    aliases: ["cash","money",'credit','c','*cash'],
 };
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
 exports.help = {
