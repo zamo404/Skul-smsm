f40ca8afd62a12aa14116ba3cfa57b73a7abae3f
Glitch (bejewled-halved-stock) <none>
2021-10-24 19:29:14 +0000

    🙍🚝 Checkpoint ./beercode.js:28757366/6684:33112061/1074

--- beercode.js
+++ beercode.js
@@ -62,102 +62,54 @@ client.on("guildDelete", guild => {
 });
 //================================================================================\\
  
-client.on('message', async message => {
-    var moment = require("moment");
-    if (message.content.startsWith("user")) {
-        if (message.author.bot) return;
-        if (message.channel.type == "dm") return message.channel.send(
-            new Discord.MessageEmbed()
-            .setColor("RED")
-            .setDescription("❌" + ` **You Can't Use This Command In DM's!**`)
-            .setFooter(client.user.username, client.user.avatarURL({ dynamic: true }))
-            .setTimestamp()
-        )
-        var args = message.content.split(" ").slice(1);
-        let user = message.mentions.users.first() || message.author || message.guild.member.cache.get(args[1]);
-        moment.locale('ar-TN');
-        const db = require('quick.db')
-        const flags = user.flags || await user.fetchFlags();
-        await db.set(`${user.id}`, [])
-        if (flags.toArray().includes('DISCORD_PARTNER')) db.push(`${user.id}`, "<:6714discordpartner:839529122467938375>")
-        if (flags.toArray().includes('HYPESQUAD_EVENTS')) db.push(`${user.id}`, "<:hypesquadbadge:839529122472656977>");
-        if (flags.toArray().includes('HOUSE_BRILLIANCE')) db.push(`${user.id}`, "<:briliance:839529152414744588>");
-        if (flags.toArray().includes('HOUSE_BRAVERY')) db.push(`${user.id}`, "<:bravery:839529152071729192>");
-        if (flags.toArray().includes('HOUSE_BALANCE')) db.push(`${user.id}`, "<:balance:839529122756952074>");
-        if (flags.toArray().includes('BUGHUNTER_LEVEL_2')) db.push(`${user.id}`, "<:hunterlv2:839529122900475964>");
-        if (flags.toArray().includes('BUGHUNTER_LEVEL_1')) db.push(`${user.id}`, "<:hunterlv1:839529122912010282>");
-        if (flags.toArray().includes('EARLY_SUPPORTER')) db.push(`${user.id}`, "<:earlysupporter:839529152100565032>");
-        if (flags.toArray().includes('VERIFIED_DEVELOPER')) db.push(`${user.id}`, "<:9developer:839529122878455848>");
-        if (flags.toArray().includes('EARLY_VERIFIED_DEVELOPER')) db.push(`${user.id}`, "<:9developer:839529122878455848>");
-        let nitro = user.avatarURL({ dynamic: true });
-        if (nitro.includes('gif')) db.push(`${user.id}`, "<:nitro:839616875184783411>");
-        var emojis = db.get(`${user.id}`);
-        var statusFull;
-        var status = user.presence.status;
-        if (status.includes('dnd')) statusFull = '🔴 | DND';
-        if (status.includes('offline')) statusFull = '⚫ | Offline';
-        if (status.includes('online')) statusFull = '🟢 | Online';
-        if (status.includes('idle')) statusFull = '🟡 | Idle';
-        var bot = false;
-        if (user.bot) bot = true;
-        message.channel.send(
-            new Discord.MessageEmbed()
-            .setAuthor(user.username, user.avatarURL({ dynamic: true }))
-            .setColor(message.member.displayHexColor)
-            .addFields({
-                name: "**Name:**",
-                value: user.username,
-                inline: true
-            }, {
-                name: "**Tag:**",
-                value: '#' + user.discriminator,
-                inline: true
-            }, {
-                name: "**Id:**",
-                value: user.id,
-                inline: true
-            }, {
-                name: "**Badge:**",
-                value: emojis,
-                inline: true
-            }, {
-                name: "**User Presence:**",
-                value: statusFull,
-                inline: true
-            }, {
-                name: "**Bot:**",
-                value: bot,
-                inline: true
-            }, {
-                name: "**Joined Discord:**",
-                value: `${moment(user.createdTimestamp).format('YYYY/M/D')} **\n** \`${moment(user.createdTimestamp).fromNow()}\``,
-                inline: true
-            }, {
-                name: "**Joined Server:**",
-                value: `${moment(user.joinedAt).format('YYYY/M/D')} \n \`${moment(user.joinedAt).fromNow()}\``,
-                inline: true
-            })
-            .setThumbnail(user.avatarURL({
-                dynamic: true,
-                format: 'png',
-                size: 1024
-            }))
-            .setFooter(client.user.username, client.user.avatarURL({ dynamic: true }))
-            .setTimestamp()
-        )
-    }
-})
-
-//================================================================================\\
-client.on("message", message => {
-  if (message.content === ";invite") {
-    let embed = new Discord.MessageEmbed()
-    .setColor("")
-    .setTitle("Invite the bot!")
-    .setDescription(`[invite]()`)
-    message.channel.send(embed);
+client.on("message", prof => {
+  if (prof.content.startsWith("server")) {
+    var professor = new Discord.MessageEmbed()
+      .setAuthor(prof.guild.name)
+      .setThumbnail(prof.guild.iconURL())
+      .setTitle("**Info Server**")
+      .addField(" ⚠️| **Server Name:**", `${prof.guild.name}`)
+      .addField(" 👑| **Owner Server:**", `${prof.guild.owner}`)
+      .addField(" 🔰| **Server ID:**", `${prof.guild.id}`)
+      .addField(" ✅| **Created:**", `${prof.guild.createdAt.toLocaleString()}`)
+      .addField(" 🚦| **Members:**", `${prof.guild.memberCount}`)
+      .addField(" 🔳| **Channels:**", `${prof.guild.channels.cache.size}`)
+      .addField(" 🌐| **Region**:", `${prof.guild.region}`)
+      .addField(" **Roles:**", ` ${prof.guild.roles.cache.size}`)
+      .setFooter(`Requested | ${prof.author.tag}`, prof.author.avatarURL())
+      .setTimestamp();
+    prof.channel.send(professor);
   }
 });
+
+//================================================================================\\
+
+client.on('message' , message => {
+
+ 
+
+  
+  if (message.content === "Jinvite") {
+
+        if(!message.channel.guild) return message.reply('**this command only for server**');
+
+     const embed = new Discord.MessageEmbed()
+
+ .setColor("RANDOM")
+
+ .setThumbnail(client.user.avatarURL("https://cdn.discordapp.com/icons/838113441008058388/a_a227ea131a1fc6b0d6d58925b6c3e2e6.gif?size=1024"))     
+ .setDescription("Invite Bot" + `
+ 
+[Link Invite](https://discord.com/oauth2/authorize?client_id=518848646016401434&permissions=414464728128&scope=bot)
+`);
+
+  message.channel.send({embed});
+
+   }
+
+});
+
+
 //================================================================================\\
 
 
@@ -197,30 +149,8 @@ client.on("message", message => {
 
 //================================================================================\\
 
-client.on('message' , message => {
 
- 
 
-  
-  if (message.content === "Jinvite") {
-
-        if(!message.channel.guild) return message.reply('**this command only for server**');
-
-     const embed = new Discord.MessageEmbed()
-
- .setColor("RANDOM")
-
- .setThumbnail(client.user.avatarURL("https://cdn.discordapp.com/icons/838113441008058388/a_a227ea131a1fc6b0d6d58925b6c3e2e6.gif?size=1024"))     
- .setDescription("Invite Bot" + `
- 
-[Link Invite](https://discord.com/oauth2/authorize?client_id=518848646016401434&permissions=414464728128&scope=bot)
-`);
-
-  message.channel.send({embed});
-
-   }
-
-});
 //===============================================================================\\
 client.on("message", async message => {
   if (message.channel.type === "dm") return;
