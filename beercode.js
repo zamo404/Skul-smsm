a961473a6b99334de35f42c0bb5f7e2aab2fe699
Glitch (bejewled-halved-stock) <none>
2021-10-24 16:43:53 +0000

    🌚⛄️ Checkpoint ./beercode.js:28757366/22028

--- beercode.js
+++ beercode.js
@@ -61,6 +61,74 @@ client.on("guildDelete", guild => {
 <:IconStaff:897811847728754718> **Member Count**: ${guild.memberCount}**`);
 });
 //================================================================================\\
+
+client.on('message', async message => {
+    if (message.content === 'discord.js-v12') {
+        let pages = [
+            `**
+الكلام الي تبيه
+    **`,
+            `**
+الكلام الي تبيه
+    **`,
+            `**
+الكلام الي تبيه
+    **`,
+            `**
+الكلام الي تبيه
+    **`, `**
+الكلام الي تبيه
+    **`
+        ];
+        let page = 1;
+ 
+        let embed = new Discord.MessageEmbed()
+            .setColor('00e8ff')
+            .setFooter(`Page ${page} of ${pages.length}`)
+            .setDescription(pages[page - 1]);
+ 
+        message.channel.send(embed).then(msg => {
+            msg.react('◀').then(r => {
+                msg.react('▶');
+ 
+                const backwardsFilter = (reaction, user) =>
+                    reaction.emoji.name === '◀' && user.id === message.author.id;
+                const forwardsFilter = (reaction, user) =>
+                    reaction.emoji.name === '▶' && user.id === message.author.id;
+ 
+                const backwards = msg.createReactionCollector(backwardsFilter, {
+                    time: 2000000
+                });
+                const forwards = msg.createReactionCollector(forwardsFilter, {
+                    time: 2000000
+                });
+ 
+                backwards.on('collect', r => {
+                    if (page === 1) return;
+                    page--;
+                    embed.setTitle('**Bot Orders**');
+                    embed.setDescription(pages[page - 1]);
+                    embed.setFooter(`Page ${page} of ${pages.length}`);
+                    msg.edit(embed);
+                    r.users.remove(message.author.id)
+                });
+                forwards.on('collect', r => {
+                    if (page === pages.length) return;
+                    page++;
+                    embed.setTitle('**Bot Orders**');
+                    embed.setDescription(pages[page - 1]);
+                    embed.setFooter(`Page ${page} of ${pages.length}`);
+                    msg.edit(embed);
+                    r.users.remove(message.author.id)
+                });
+            });
+        });
+    }
+});
+
+//================================================================================\\
+
+ 
 client.on("message", message => {
   if (message.content === "Jhelp") {
     const embed = new Discord.MessageEmbed()
