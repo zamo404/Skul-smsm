9b27da6e229578bfb1b21f223d1b49e29bbe5045
Glitch (bejewled-halved-stock) <none>
2021-10-26 09:34:28 +0000

    ✌️🔋 Checkpoint ./beercode.js:33112061/20

--- beercode.js
+++ beercode.js
@@ -102,17 +102,13 @@ if(message.content == 'Jmembers') {
 const embed = new Discord.MessageEmbed()
  .setColor("RANDOM")
 .setDescription(`**Member cases <:Servers:902312906618724372>
-
 <:onlines:895398659389292625> Online ${message.guild.members.cache.filter(m=>m.presence.status == 'online').size}
 <:dnd:895398659217309737> Do Not Disturb ${message.guild.members.cache.filter(m=>m.presence.status == 'dnd').size}
 <:sleep:895398659284434945> Idle ${message.guild.members.cache.filter(m=>m.presence.status == 'idle').size}   
 <:Offlines:895398658969862245> Offline ${message.guild.members.cache.filter(m=>m.presence.status == 'offline').size} 
 <:Users:902312879838068796> All ${message.guild.memberCount}**`)
-
 message.channel.send(embed)
-  
-
-}
+    }
 });
 
 //================================================================================\\
