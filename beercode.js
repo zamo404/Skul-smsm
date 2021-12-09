5fbca182342831a74440fd77c91e72076639b9d8
Glitch (bejewled-halved-stock) <none>
2021-12-09 17:59:01 +0000

    🐪🐦 Checkpoint ./beercode.js:33112061/1499:39790114/25973

--- beercode.js
+++ beercode.js
@@ -17,9 +17,12 @@ require("./util/eventLoader.js")(client);
 
 //===============================================================================\\
 client.on("ready", async () => {
-  client.user.setActivity(`Jhelp|Verify Soon|Server,${client.guilds.cache.size}`, {
-    type: "PLAYING"
-  });
+  client.user.setActivity(
+    `Jhelp|Verify Soon|Server,${client.guilds.cache.size}`,
+    {
+      type: "PLAYING"
+    }
+  );
 });
 //===============================================================================\\
 client.on("guildCreate", guild => {
@@ -42,62 +45,71 @@ client.on("guildDelete", guild => {
 
 //================================================================================\\
 
-client.on('message' , message => {
+client.on("message", message => {
   if (message.content === "Jinvite") {
+    if (!message.channel.guild)
+      return message.reply("**this command only for server**");
 
-        if(!message.channel.guild) return message.reply('**this command only for server**');
+    const embed = new Discord.MessageEmbed()
 
-     const embed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
 
- .setColor("RANDOM")
-
- .setThumbnail(client.user.avatarURL("https://cdn.discordapp.com/icons/838113441008058388/a_a227ea131a1fc6b0d6d58925b6c3e2e6.gif?size=1024"))     
- .setDescription("Invite Bot" + `
+      .setThumbnail(
+        client.user.avatarURL(
+          "https://cdn.discordapp.com/icons/838113441008058388/a_a227ea131a1fc6b0d6d58925b6c3e2e6.gif?size=1024"
+        )
+      )
+      .setDescription(
+        "Invite Bot" +
+          `
  
 [Link Invite](https://discord.com/oauth2/authorize?client_id=518848646016401434&permissions=414464728128&scope=bot)
-`);
+`
+      );
 
-  message.channel.send({embed});
-
-   }
-
+    message.channel.send({ embed });
+  }
 });
 //================================================================================\\
-client.on('message' , message => {
+client.on("message", message => {
   if (message.content === "Jsupport") {
+    if (!message.channel.guild)
+      return message.reply("**this command only for server**");
 
-        if(!message.channel.guild) return message.reply('**this command only for server**');
+    const embed = new Discord.MessageEmbed()
 
-     const embed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
 
- .setColor("RANDOM")
-
- .setThumbnail(client.user.avatarURL("https://cdn.discordapp.com/icons/838113441008058388/a_a227ea131a1fc6b0d6d58925b6c3e2e6.gif?size=1024"))     
- .setDescription("Support Server" + `
+      .setThumbnail(
+        client.user.avatarURL(
+          "https://cdn.discordapp.com/icons/838113441008058388/a_a227ea131a1fc6b0d6d58925b6c3e2e6.gif?size=1024"
+        )
+      )
+      .setDescription(
+        "Support Server" +
+          `
  
 [Link Server](https://discord.gg/a9QvFY7te7)
-`);
+`
+      );
 
-  message.channel.send({embed});
-
-   }
-
+    message.channel.send({ embed });
+  }
 });
 //================================================================================\\
-client.on('message',async message => {
-  if(message.content.startsWith("Juptime")) { 
-    let rozh= Math.floor(client.uptime / 86400000);
-    let katzhmer= Math.floor(client.uptime / 3600000) % 24;
-    let daqa= Math.floor(client.uptime / 60000) % 60;
-    let chrka= Math.floor(client.uptime / 1000) % 60;
- 
-    return message.channel.send(`__Uptime:__\n${rozh}d ${katzhmer}h ${daqa}m ${chrka}s`);
+client.on("message", async message => {
+  if (message.content.startsWith("Juptime")) {
+    let rozh = Math.floor(client.uptime / 86400000);
+    let katzhmer = Math.floor(client.uptime / 3600000) % 24;
+    let daqa = Math.floor(client.uptime / 60000) % 60;
+    let chrka = Math.floor(client.uptime / 1000) % 60;
+
+    return message.channel.send(
+      `__Uptime:__\n${rozh}d ${katzhmer}h ${daqa}m ${chrka}s`
+    );
   }
- 
-})
+});
 //================================================================================\\
-
-
 
 //================================================================================\\
 
@@ -115,92 +127,92 @@ client.on("message", message => {
 //================================================================================\\
 
 client.on(`message`, message => {
-    if (message.content.startsWith("Jserver")) {
-        if (!message.channel.guild) return message.channel.send('This is for servers only');
- 
-        const text = message.guild.channels.cache.filter(r => r.type === "text").size
-        const voice = message.guild.channels.cache.filter(r => r.type === "voice").size
-        const chs = message.guild.channels.cache.size
-        const avaibles = message.guild.features.map(features => features.toString()).join("\n")
- 
-        const roles = message.guild.roles.cache.size
- 
-        const online = message.guild.members.cache.filter(m =>
-            m.presence.status === 'online'
-        ).size
- 
-        const idle = message.guild.members.cache.filter(m =>
-            m.presence.status === 'idle'
-        ).size
- 
-        const offline = message.guild.members.cache.filter(m =>
-            m.presence.status === 'offline'
-        ).size
- 
-        const dnd = message.guild.members.cache.filter(m =>
-            m.presence.status === 'dnd'
-        ).size
- 
-        const black = new Discord.MessageEmbed()
-            .setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
-            .setColor('BLACK')
-            .addFields({
-                name: `🆔 Server ID`,
-                value: `${message.guild.id}`,
-                inline: true
- 
-            }, {
-                name: `📆 Created On`,
-                value: moment(message.guild.createdAt).format("YYYY/MM/DD, HH:mm") + '\n' + moment(message.guild.createdAt, "YYYYMMDD").fromNow(),
-                inline: true
-            }, {
-                name: `👑 Owner By`,
-                value: `${message.guild.owner}`,
-                inline: true
- 
-            }, {
-                name: `👥 Members (${message.guild.memberCount})`,
-                value: `**${online}** Online \n **${message.guild.premiumSubscriptionCount}** Boosts ✨`,
-                inline: true
-            }, {
-                name: `💬 Channels (${chs})`,
-                value: `**${text}** Text | **${voice}** Voice`,
-                inline: true
-            }, {
-                name: `🌍 Others`,
-                value: `**Region:** ${message.guild.region}\n**Verification Level:** ${message.guild.mfaLevel}`,
-                inline: true
-            }, {
-                name: `🔐 Roles (${roles})`,
-                value: `To see a list with all roles use #roles`,
-                inline: true
-            }, )
-            .setFooter('Developer Support', message.author.avatarURL())
-        message.channel.send(black)
-    }
+  if (message.content.startsWith("Jserver")) {
+    if (!message.channel.guild)
+      return message.channel.send("This is for servers only");
+
+    const text = message.guild.channels.cache.filter(r => r.type === "text")
+      .size;
+    const voice = message.guild.channels.cache.filter(r => r.type === "voice")
+      .size;
+    const chs = message.guild.channels.cache.size;
+    const avaibles = message.guild.features
+      .map(features => features.toString())
+      .join("\n");
+
+    const roles = message.guild.roles.cache.size;
+
+    const online = message.guild.members.cache.filter(
+      m => m.presence.status === "online"
+    ).size;
+
+    const idle = message.guild.members.cache.filter(
+      m => m.presence.status === "idle"
+    ).size;
+
+    const offline = message.guild.members.cache.filter(
+      m => m.presence.status === "offline"
+    ).size;
+
+    const dnd = message.guild.members.cache.filter(
+      m => m.presence.status === "dnd"
+    ).size;
+
+    const black = new Discord.MessageEmbed()
+      .setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
+      .setColor("BLACK")
+      .addFields(
+        {
+          name: `🆔 Server ID`,
+          value: `${message.guild.id}`,
+          inline: true
+        },
+        {
+          name: `📆 Created On`,
+          value:
+            moment(message.guild.createdAt).format("YYYY/MM/DD, HH:mm") +
+            "\n" +
+            moment(message.guild.createdAt, "YYYYMMDD").fromNow(),
+          inline: true
+        },
+        {
+          name: `👑 Owner By`,
+          value: `${message.guild.owner}`,
+          inline: true
+        },
+        {
+          name: `👥 Members (${message.guild.memberCount})`,
+          value: `**${online}** Online \n **${message.guild.premiumSubscriptionCount}** Boosts ✨`,
+          inline: true
+        },
+        {
+          name: `💬 Channels (${chs})`,
+          value: `**${text}** Text | **${voice}** Voice`,
+          inline: true
+        },
+        {
+          name: `🌍 Others`,
+          value: `**Region:** ${message.guild.region}\n**Verification Level:** ${message.guild.mfaLevel}`,
+          inline: true
+        },
+        {
+          name: `🔐 Roles (${roles})`,
+          value: `To see a list with all roles use #roles`,
+          inline: true
+        }
+      )
+      .setFooter("Developer Support", message.author.avatarURL());
+    message.channel.send(black);
+  }
 });
 
 //================================================================================\\
 
+//================================================================================\\
 
 //================================================================================\\
 
-client.on('guildCreate', guild => {
-var embed = new Discord.RichEmbed()
-  .setColor("WHITE")
-  .setDescription(`
-> Thank you for adding me 
-> My Prefix : \`J\`
-> For Help type : \`Jhelp\``)
-      guild.owner.send(embed)
-});
-
 //================================================================================\\
-
-
-
-//================================================================================\\
-
 
 client.on("message", message => {
   if (message.content === "Jhelp") {
@@ -226,27 +238,30 @@ client.on("message", message => {
   }
 });
 //================================================================================\\
-client.on('message', message => {
-    if(message.content === "Jabout") {
-        const embed = new Discord.MessageEmbed()
-        .setColor("ORANGE")
-        .setDescription(`
+client.on("message", message => {
+  if (message.content === "Jabout") {
+    const embed = new Discord.MessageEmbed().setColor("ORANGE").setDescription(`
  
 **__About Bot__**
  
 <:Servers:902312906618724372> **Servers** - **__${client.guilds.cache.size}__**
 ✧･ﾟ: ✧･ﾟ: - :･ﾟ✧:･ﾟ
-<:Users:902312879838068796> **Users** - **__${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}__**
+<:Users:902312879838068796> **Users** - **__${client.guilds.cache.reduce(
+      (a, g) => a + g.memberCount,
+      0
+    )}__**
 ✧･ﾟ: ✧･ﾟ: - :･ﾟ✧:･ﾟ
-<:channels:895398664468594739> **Channels** - **__${client.channels.cache.size}__**
+<:channels:895398664468594739> **Channels** - **__${
+      client.channels.cache.size
+    }__**
 ✧･ﾟ: ✧･ﾟ: - :･ﾟ✧:･ﾟ
 <:owner:902312748371820594> **Owner Bot** - <@349942964904001546>
 ✧･ﾟ: ✧･ﾟ: - :･ﾟ✧:･ﾟ
 <:Devloper:902181949110636574> **Admin Bot** - <@800994896890691605>
 
-`)
-               message.channel.send(embed);
-           }
+`);
+    message.channel.send(embed);
+  }
 });
 //===============================================================================\\
 client.on("message", async message => {
@@ -257,14 +272,18 @@ client.on("message", async message => {
     message.member = await message.guild.fetchMember(message);
 
   if (message.content.match(new RegExp(`^<@!?${client.user.id}>`))) {
-    return message.channel.send(`Hi Im <@518848646016401434> My prefix is { J }`);
+    return message.channel.send(
+      `Hi Im <@518848646016401434> My prefix is { J }`
+    );
   }
 });
 //===============================================================================\\
-client.on('ready', async() => {
-        client.channels.cache.get('903900364363956235').send("Hello Im Online <a:logserver:891321228537782313>")
-        client.channels.cache.get('903910230444871680').join()
-        console.log("Hello Im Online")
+client.on("ready", async () => {
+  client.channels.cache
+    .get("903900364363956235")
+    .send("Hello Im Online <a:logserver:891321228537782313>");
+  client.channels.cache.get("903910230444871680").join();
+  console.log("Hello Im Online");
 });
 //===============================================================================\\
 client.commands = new Discord.Collection();
@@ -273,8 +292,6 @@ fs.readdir("./Destek/", (err, files) => {
   if (err) console.error(err);
   log(`${files.length} Installing support command...`);
   files.forEach(f => {
-   
-
     let props = require(`./Destek/${f}`);
     log(`Support Command Installed: ${props.help.name}.`);
     client.commands.set(props.help.name, props);
@@ -306,8 +323,6 @@ fs.readdir("./Ekonomi/", (err, files) => {
   if (err) console.error(err);
   log(`${files.length} Economy command Loading ...`);
   files.forEach(f => {
-    
-
     let props = require(`./Ekonomi/${f}`);
     log(`Economy Command Installed: ${props.help.name}.`);
     client.commands.set(props.help.name, props);
@@ -316,7 +331,6 @@ fs.readdir("./Ekonomi/", (err, files) => {
     });
   });
 });
-
 
 client.load = command => {
   return new Promise((resolve, reject) => {
@@ -333,7 +347,6 @@ client.load = command => {
   });
 };
 //===============================================================================\\
-
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
@@ -365,68 +378,12 @@ client.load = command => {
   });
 };
 
-const members = function server.members.fetch({ limit: 3000 })
-if (members.size < 3000)
-
-
-const smsm = ["349942964904001546"] //id xot dane
- 
-client.on('message', message => {
-    var argresult = message.content.split( ).slice(1).join(' ');
- 
- 
-client.on('message', message => {
-    var argresult = message.content.split( ).slice(1).join(' ');
-      if (!smsm.includes(message.author.id)) return;
- 
- 
-    if (message.content === ("Jleave")) {
-    message.guild.leave();        
- message.channel.send('TAWAW LEFTM KRD')
- 
- 
-    } 
- 
-})
- 
- 
-})
-
-
-client.on("message", async(NotOurs) => {
- 
-  if (NotOurs.author.bot) return;
-let devs = ["820702223847129138"];
-  if (NotOurs.content.toLowerCase() ===  "Jlinks") {
-      if(!devs.includes(NotOurs.author.id)){
-    let embed = new Discord.MessageEmbed()
-    .setColor("RANDOM")
-    .setTitle("**ليس لديك صلاحيات**");
-    NotOurs.channel.send(embed).then( z => z.delete({timeout:3000}));
- 
-  } 
-    client.guilds.cache.forEach(g => {
- 
-      let l = g.id;
-                g.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(g.me).has('SEND_MESSAGES'))
-//g.channels.cache.get(g.channels.first().id)
-        .createInvite({
-          maxUses: 100,
-          maxAge: 86400
-        })
-        .then(i =>
-          NotOurs.channel.send(`
-        https://discord.gg/${i.code}
-        [ ${g.owner} ]
-         
-       ` )
-        ); 
-    });
+client.on("guildCreate", server => {
+  if (server.members.cache.size < 3000) {
+    server.leave();
+    console.log("Leave Server -3000 member");
   }
-}); 
-
-
-
+});
 
 //====================================================//
 client.login(process.env.TOKEN_BOT);
